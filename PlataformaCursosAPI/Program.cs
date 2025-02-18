40e6da9f9e58c40929920d32ec72ff3f5644dc22
João Lemes <joaolemes17@hotmail.com>
2025-02-18 14:01:30 -0400

    fix: corrigindo erros

--- PlataformaCursosAPI/Program.cs
+++ PlataformaCursosAPI/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-using PlataformaCursosAPI.Models;
 using PlataformaCursosAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona o DbContext
+// Adiciona o DbContext para MySQL
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 30)) // Ajuste para a versão do seu MySQL
+    ));
 
 // Adiciona os serviços de controllers e Swagger
 builder.Services.AddControllers();
@@ -23,7 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
